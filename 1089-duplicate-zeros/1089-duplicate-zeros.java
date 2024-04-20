4e51e792744b8ccda18cfb21aa9895d824809d87
yamwoong <100250784+yamwoong@users.noreply.github.com>
2024-04-20 22:03:27 +0900

    Time: 1 ms (97.64%), Space: 44.7 MB (39.04%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,17 +1,25 @@
 class Solution {
-  public void duplicateZeros(int[] arr) {
-    int zeros = 0;
-
-    for (int a : arr)
-      if (a == 0)
-        zeros++;
-
-    for (int i = arr.length - 1, j = arr.length + zeros - 1; i < j; i--, j--) {
-      if (j < arr.length)
-        arr[j] = arr[i];
-      if (arr[i] == 0)
-        if (--j < arr.length)
-          arr[j] = arr[i];
+    public void duplicateZeros(int[] arr) {
+        int zeroes = 0;
+        for (int i: arr) {
+            if (i == 0) {
+                zeroes++;
+            }
+        }
+        int i = arr.length - 1, j = arr.length + zeroes - 1;
+        
+        while (i != j) {
+            insert(arr, i, j--);
+            if (arr[i] == 0) {
+                insert(arr, i, j--);
+            }
+            i--;
+        }
     }
-  }
+    
+    private void insert(int[] arr, int i, int j) {
+        if (j < arr.length) {
+            arr[j] = arr[i];
+        }
+    }
 }
