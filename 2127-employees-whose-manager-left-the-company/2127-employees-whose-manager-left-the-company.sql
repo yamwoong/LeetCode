b14634337a00643f6a16f7a9f3ab6a09422853f1
Dae Woong Kim <100250784+yamwoong@users.noreply.github.com>
2024-04-30 09:19:12 +0900

    Time: 1034 ms (41.38%), Space: 0B (100%) - LeetHub

---
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,8 @@
+SELECT  EMPLOYEE_ID
+  FROM  EMPLOYEES
+ WHERE  SALARY < '30000'
+   AND  MANAGER_ID NOT IN (
+                            SELECT  EMPLOYEE_ID
+                              FROM  EMPLOYEES
+                          )
+ ORDER BY EMPLOYEE_ID
