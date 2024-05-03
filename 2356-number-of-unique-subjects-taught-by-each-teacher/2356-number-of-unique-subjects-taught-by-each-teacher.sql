e67ad23f9bea6de5a20358f510acfd9089d8e8a4
Dae Woong Kim <100250784+yamwoong@users.noreply.github.com>
2024-05-03 23:39:35 +0900

    Time: 970 ms (61.78%), Space: 0B (100%) - LeetHub

---
+++ 2356-number-of-unique-subjects-taught-by-each-teacher/2356-number-of-unique-subjects-taught-by-each-teacher.sql
@@ -0,0 +1,9 @@
+SELECT  TEACHER_ID
+     ,  COUNT(SUBJECT_ID)   AS CNT
+  FROM  (
+            SELECT  TEACHER_ID
+                 ,  SUBJECT_ID
+              FROM  TEACHER
+             GROUP BY TEACHER_ID, SUBJECT_ID 
+        )
+ GROUP BY TEACHER_ID
